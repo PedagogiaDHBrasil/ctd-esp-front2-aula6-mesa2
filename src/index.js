6339a37bd58ff0ef01eca9390862ba13c66f4715
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-04-01 14:45:34 -0300

    :label: importando arquivo tsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import Reducer from "./Reducer";
+import Reducer from "./Reducer.tsx";
 import App from "./App";
 import "./index.css";
 
